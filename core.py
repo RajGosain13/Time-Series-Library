913b7935b11e6f36c994c8e9ec3ffb2ac85017c3
RajGosain13 <rgosain3@jhu.edu>
2025-06-15 03:26:20 -0500

    added pacf and acf helper method

--- core.py
+++ core.py
@@ -28,7 +28,7 @@ def variance_function(time_series: np.ndarray) -> float:
 
 def autocovariance_function(time_series: np.ndarray, lag: int) -> float:
     '''
-    Find the autocovariance at the input lag
+    Finds the autocovariance at the input lag
 
     Parameters:
         time_series (np.ndarray): The input time series
@@ -44,7 +44,7 @@ def autocovariance_function(time_series: np.ndarray, lag: int) -> float:
 
 def autocorrelation_function(time_series: np.ndarray, lag: int) -> float:
     '''
-    Find the autocorrelation at the input lag
+    Finds the autocorrelation at the input lag
 
     Parameters:
         time_series (np.ndarray): The input time series
@@ -59,3 +59,40 @@ def autocorrelation_function(time_series: np.ndarray, lag: int) -> float:
         return np.nan
     return autocovariance_function(time_series, lag) / acvf0
 
+def __acf__(time_series: np.ndarray, lags: int) -> np.ndarray:
+    return np.array([autocorrelation_function(time_series, k) for k in range(lags + 1)])
+
+def pacf(time_series: np.ndarray, lags: int, return_all_phi: bool = False):
+    '''
+    Finds the partial autocorrelation at the given input lags
+
+    Parameters:
+        time_series (np.ndarray): The input time series
+        lag (int): The lags at which we compute autocorrelation
+        return_all_phi (bool): If True, returns all phi[k, j]
+    '''
+    acf_vals = __acf__(time_series, lags)
+    pacf_vals = np.zeros(lags+1)
+    phi = np.zeros((lags+1, lags+1))
+    variance = np.zeros(lags+1)
+
+    pacf_vals[0] = 1.0
+    phi[1,1] = acf_vals[1]
+    variance_function[1] = 1 - np.power(acf_vals, 2)
+    pacf_vals[1] = phi[1,1,]
+
+    for k in range(2, lags+1):
+        sum_phi_gamma = sum(phi[k-1,j] * acf_vals[k-j] for j in range(1, k))
+        phi[k, k] = (acf_vals[k] - sum_phi_gamma) / variance[k - 1]
+
+        for j in range(1, k):
+            phi[k, j] = phi[k - 1, j] - phi[k, k] * phi[k - 1, k - j]
+
+        variance[k] = variance[k - 1] * (1 - phi[k, k]**2)
+        pacf_vals[k] = phi[k, k]\
+        
+    if return_all_phi:
+        phi_table = {f'phi_{k}_{j}': phi[k, j] for k in range(1, lags + 1) for j in range(1, k + 1)}
+        return pacf_vals, phi_table
+    
+    return pacf_vals
